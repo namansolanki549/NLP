de032f1b55b107af2a6a1dfa3d4d98f1faa3c2cd
Naman Solanki <namansolanki549@gmail.com>
2020-12-04 14:41:36 +0530

    Add files via upload

---
+++ Projects/SMSSPamClassifier/project_spam_classifier.py
@@ -0,0 +1,45 @@
+import pandas as pd
+
+messages = pd.read_csv('C:/Users/naman solanki/Desktop/NLP/smsspamcollection/SMSSpamCollection', sep = '\t', names = ['labels', 'message'])
+
+import re
+from nltk.corpus import stopwords
+from nltk.stem.porter import PorterStemmer
+from nltk.stem import WordNetLemmatizer
+
+ps = PorterStemmer()
+wordnet=WordNetLemmatizer()
+
+corpus = []
+for i in range(len(messages)):
+    review = re.sub('[^a-zA-Z]', ' ', messages['message'][i])
+    review = review.lower()
+    review = review.split()
+    review = [ps.stem(word) for word in review if not word in set(stopwords.words('english'))]
+    review = ' '.join(review)
+    corpus.append(review)
+
+from sklearn.feature_extraction.text import CountVectorizer
+cv = CountVectorizer(max_features = 5000)
+X = cv.fit_transform(corpus).toarray()
+
+
+y = pd.get_dummies(messages['labels'])
+y = y.iloc[:,1].values
+
+
+from sklearn.model_selection import train_test_split
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.20, random_state = 0)
+
+
+from sklearn.naive_bayes import MultinomialNB
+spam_detect_model = MultinomialNB().fit(X_train, y_train)
+
+y_pred = spam_detect_model.predict(X_test)
+
+from sklearn.metrics import confusion_matrix
+confusion_m = confusion_matrix(y_test, y_pred)
+
+
+from sklearn.metrics import accuracy_score
+accuracy = accuracy_score(y_test, y_pred)
